3087749beb43e3f8e602c206ead7617c4d5c9d62
ikrom <ikhakimov3@gmail.com>
2025-09-27 18:53:52 +0500

    Lesson 3.7

---
+++ intermediate/structs.go
@@ -0,0 +1,81 @@
+package intermediate
+
+import "fmt"
+
+type Person struct {
+	firstName string
+	lastName  string
+	age       int
+	address   Address
+	PhoneHomeCell
+}
+
+type PhoneHomeCell struct {
+	home string
+	cell string
+}
+
+type Address struct {
+	city    string
+	country string
+}
+
+func (p Person) fullName() string {
+	return p.firstName + " " + p.lastName
+}
+
+func (p *Person) incrementAgeByOne() {
+	p.age++
+}
+
+type Parent struct {
+	firstName string
+}
+
+func main() {
+
+	p1 := Person{
+		firstName: "John",
+		lastName:  "Doe",
+		age:       30,
+		address: Address{
+			city:    "London",
+			country: "U.K",
+		},
+		PhoneHomeCell: PhoneHomeCell{
+			home: "4612312321",
+			cell: "1231231231",
+		},
+	}
+
+	p2 := Person{
+		firstName: "Jane",
+		age:       25,
+	}
+
+	p3 := Person{
+		firstName: "Jane",
+		age:       25,
+	}
+
+	fmt.Println(p1.firstName)
+	fmt.Println(p2.firstName)
+	fmt.Println(p2.fullName())
+	fmt.Println(p1)
+	fmt.Println("Are p2 and p3 equal:", p2 == p3)
+
+	// Anonymous Structs
+	user := struct {
+		username string
+		email    string
+	}{
+		username: "user123",
+		email:    "pseudoemail@example.org",
+	}
+
+	fmt.Println(user.username)
+	fmt.Println("Before increment", p1.age)
+	p1.incrementAgeByOne()
+	fmt.Println("After increment", p1.age)
+
+}
